127119021680c0b523c517751d58483dc109f608
asapzacy <zacdarellano@gmail.com>
2018-12-31 13:29:53 -0700

    Add create new github repo script

---
+++ create-github-repo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USERNAME='asapzacy'
+REPO_NAME=$1
+
+mkdir $REPO_NAME
+cd $REPO_NAME
+
+curl -u $USERNAME https://api.github.com/user/repos -d "{\"name\": \"$REPO_NAME\"}"
+
+git init
+echo "# $REPO_NAME" > README.md
+echo -e ".DS_Store \n .vscode/ \n\n .env \n .ssl/ \n .node_modules/ \n yarn-error.log" > .gitignore
+git add .
+git commit -m "Initial commit"
+git remote add origin git@github.com:$USERNAME/$REPO_NAME.git
+git push -u origin master
