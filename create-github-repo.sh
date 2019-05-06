719edb0b0e08e4ebcea5b59f79794a8f8f31137c
Zac Arellano <zacdarellano@gmail.com>
2019-05-05 17:31:21 -0700

    Update create-github-repo.sh script

--- create-github-repo.sh
+++ create-github-repo.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-USERNAME='asapzacy'
+USERNAME=asapzacy
 REPO_NAME=$1
+IS_PUBLIC=true
+
+if [[ $2 == "--private" ]]; then
+  IS_PUBLIC=false
+fi
 
 cd ~/code
-mkdir $REPO_NAME && cd $REPO_NAME
 
-curl -u $USERNAME https://api.github.com/user/repos -d "{\"name\": \"$REPO_NAME\"}"
+if [ -d "$REPO_NAME" ]; then
+  echo "'$REPO_NAME' already exists, exiting."
+  exit 1
+else
+  mkdir $REPO_NAME
+  cd $REPO_NAME
+fi
+
+curl -u $USERNAME https://api.github.com/user/repos -d "{\"name\": \"$REPO_NAME\", \"public\": \"$IS_PUBLIC\"}"
 
 git init
 echo "# $REPO_NAME" > README.md
 echo -e ".DS_Store \n .vscode/ \n\n .env \n .ssl/ \n .node_modules/ \n yarn-error.log" > .gitignore
+
 git add .
 git commit -m "Initial commit"
 git remote add origin git@github.com:$USERNAME/$REPO_NAME.git
